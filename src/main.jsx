03af4ebc99ae59dd8c2e3e1a4e2e97b65ff458aa
Shravanii Kale <109503478+Shravanikale@users.noreply.github.com>
2025-01-28 17:12:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<HashRouter>
 <ShopContextProvider>
 <App />
 </ShopContextProvider>
-</BrowserRouter>,
+</HashRouter>,
 
 )
